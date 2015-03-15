9081907fbb8f0f86d176cbe7caf400afea7906d2
Demelza <demeruza@yahoo.fr>
2015-03-15 19:11:47 +0100

    Make tree supposedly work with SlimLP rom

--- slim.mk
+++ slim.mk
@@ -1,12 +1,12 @@
 $(call inherit-product, device/oppo/find7/full_find7.mk)
 
 # Enhanced NFC
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+$(call inherit-product, vendor/slim/config/nfc_enhanced.mk)
 
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/slim/config/common_full_phone.mk)
 
-PRODUCT_NAME := cm_find7
+PRODUCT_NAME := slim_find7
 PRODUCT_DEVICE := find7
 
 PRODUCT_GMS_CLIENTID_BASE := android-oppo

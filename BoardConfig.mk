bb644e50faf5c2fa3c4e64052637a28ef7b25313
Demelza <demeruza@yahoo.fr>
2015-03-15 19:06:42 +0100

    Correct paths

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -38,7 +38,7 @@ BOARD_USERDATAIMAGE_PARTITION_SIZE := 13747929088 # 13747945472 - 16384 for cryp
 #BOARD_USERDATAIMAGE_PARTITION_SIZE := 3221225472
 
 # Recovery
-TARGET_RECOVERY_FSTAB := device/oppo/find7-common/rootdir/etc/fstab.qcom
+TARGET_RECOVERY_FSTAB := device/oppo/find7/rootdir/etc/fstab.qcom
 
 AUDIO_FEATURE_LOW_LATENCY_PRIMARY := true
 
